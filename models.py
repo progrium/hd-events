096bb161aefee6c6ceedbf568acd8d701745504b
Brent Tubbs <brent@mae.(none)>
2010-04-30 20:39:20 -0700

    Added logging to event actions

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from google.appengine.ext import db
 from google.appengine.api import urlfetch, memcache, users, mail
 from datetime import datetime, timedelta, time, date
+import logging
 
 ROOM_OPTIONS = ['cave', 'deck', 'savanna', 'frontarea', '140b']
 GUESTS_PER_STAFF = 25
@@ -11,7 +12,7 @@ def to_sentence(aList):
     sentence = ', '.join([e for e in aList if aList.index(e) != len(aList) -1])
     if len(aList) > 1: sentence = '%s and %s' % (sentence, aList[-1])
     return sentence
-    
+
 def to_name_list(aList):
     sentence = ', '.join([human_username(e) for e in aList if aList.index(e) != len(aList) -1])
     if len(aList) > 1: sentence = '%s and %s' % (sentence, human_username(aList[-1]))
@@ -26,17 +27,17 @@ class Event(db.Model):
     end_time = db.DateTimeProperty()
     staff = db.ListProperty(users.User)
     rooms = db.StringListProperty() #choices=set(ROOM_OPTIONS)
-    
+
     details = db.StringProperty()
     url = db.StringProperty()
     fee = db.StringProperty()
     notes = db.StringProperty()
     type = db.StringProperty(required=True)
     estimated_size = db.StringProperty(required=True)
-    
+
     contact_name = db.StringProperty(required=True)
     contact_phone = db.StringProperty(required=True)
-    
+
     expired = db.DateTimeProperty()
     created = db.DateTimeProperty(auto_now_add=True)
     updated = db.DateTimeProperty(auto_now=True)
@@ -47,7 +48,7 @@ class Event(db.Model):
             .filter('start_time >', datetime.today()) \
             .filter('status IN', ['approved', 'canceled']) \
             .order('start_time')
-    
+
     @classmethod
     def get_pending_list(cls):
         return cls.all() \
@@ -72,30 +73,38 @@ class Event(db.Model):
 
     def start_date(self):
         return self.start_time.date()
-    
+
     def approve(self):
+        user = users.get_current_user()
         if self.is_staffed():
             self.expired = None
             self.status = 'approved'
+            logging.info('%s approved %s' % (user.nickname, self.name))
         else:
             self.status = 'understaffed'
+            logging.info('%s approved %s but it is still understaffed' % (user.nickname, self.name))
         self.put()
-        
+
     def cancel(self):
+        user = users.get_current_user()
         self.status = 'canceled'
         self.put()
-    
+        logging.info('%s cancelled %s' % (user.nickname, self.name))
+
     def expire(self):
+        user = users.get_current_user()
         self.expired = datetime.now()
         self.status = 'expired'
         self.put()
-    
+        logging.info('%s expired %s' % (user.nickname, self.name))
+
     def add_staff(self, user):
         self.staff.append(user)
         if self.is_staffed() and self.status == 'understaffed':
             self.status = 'approved'
         self.put()
-    
+        logging.info('%s staffed %s' % (user.nickname, self.name))
+
     def to_ical(self):
         event = CalendarEvent()
         event.add('summary', self.name if self.status == 'approved' else self.name + ' (%s)' % self.status.upper())
@@ -108,4 +117,3 @@ class Feedback(db.Model):
     rating = db.IntegerProperty()
     comment = db.StringProperty(multiline=True)
     created = db.DateTimeProperty(auto_now_add=True)
-
